b42f8fa792fb798989ddb1ecd7a9b71477f58ba1
jimklo83 <jimklo83@mail.com>
2023-09-29 18:45:07 -0500

    finished excercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Exercise 1");
+            Console.WriteLine();
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"17/4 is {quotient} remainder {remainder}.");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise 2");
+            Console.WriteLine();
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radious = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with a radious of {radious} is {AreaOfCircle(radious)}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
